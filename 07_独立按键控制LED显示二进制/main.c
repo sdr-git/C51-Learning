5332d577460e3d60a1946a3fc3e9bce3ce91f797
SDR-git <2191398297@qq.com>
2026-07-09 17:23:25 +0800

    2026/7/9独立按键控制LED显示二进制

---
+++ 07_独立按键控制LED显示二进制/main.c
@@ -0,0 +1,33 @@
+#include <REGX52.H>
+
+void Delay(unsigned int xms)		//@12.000MHz
+{
+	unsigned char i, j;
+	while(xms--)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
+}
+
+void main()
+{
+	unsigned char LEDNum=0;
+	while(1)
+	{
+		if(P3_1==0)
+		{
+			Delay(20);
+			while(P3_1==0);
+			Delay(20);
+			//1111 1111
+			LEDNum++;
+			P2=~LEDNum;//1111 1111
+		}
+	}
+
+}
